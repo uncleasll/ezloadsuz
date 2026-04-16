5a9141d81e06885b4eb550570fee5512b079143d
Rakun <rakun@gmail.com>
2026-04-16 22:04:23 +0500

    some change

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+        allow_origins=[
+        "https://uzloads.onrender.com",
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
